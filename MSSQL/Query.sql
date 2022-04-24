d235096d6d03d8fa736a304a9e5fa9007a0a61c5
Alex <ssaasshha11@yandex.ru>
2022-04-24 17:50:42 +0400

    init

--- MSSQL/Query.sql
+++ MSSQL/Query.sql
@@ -1,3 +1,3 @@
 SELECT cat.Name As Categorie, prod.Name As Product FROM dbo.Categorie As cat
-JOIN dbo.[Categorie-To-Product] link ON cat.Id = link.CategorieId
-JOIN dbo.Product prod ON link.ProductId = prod.Id
+RIGHT JOIN dbo.[Categorie-To-Product] link ON cat.Id = link.CategorieId
+RIGHT JOIN dbo.Product prod ON link.ProductId = prod.Id
